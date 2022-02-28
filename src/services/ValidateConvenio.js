072ee429a927161da5c50e01541ae0406435d9b1
Tiago Carioca <tiagoacarioca@hotmail.com>
2022-02-27 23:56:26 -0400

    validate due date for invoices

--- src/services/ValidateConvenio.js
+++ src/services/ValidateConvenio.js
@@ -6,7 +6,13 @@ class ValidateConvenioService {
   validate(linhaDigitavel) {
     const value = linhaDigitavel.substr(4, 7) + linhaDigitavel.substr(12, 4);
     const amount = `${Number(value.substr(0, 9))}.${value.slice(-2)}`;
-    const dueDate = linhaDigitavel.substr(20, 3) + linhaDigitavel.substr(24, 5);
+
+    const dueDateLine =
+      linhaDigitavel.substr(20, 3) + linhaDigitavel.substr(24, 5);
+
+    const year = dueDateLine.substring(0, 4);
+    const month = dueDateLine.substring(4, 2);
+    const day = dueDateLine.substr(6, 2);
 
     const dvg = Number(linhaDigitavel[2]);
 
@@ -52,10 +58,22 @@ class ValidateConvenioService {
       throw new AppError('Boleto inválido', 400);
     }
 
+    if (year > 1970) {
+      if (month > 0 && month <= 12) {
+        if (day > 0 && day <= 31) {
+          const dueDate = new Date(`${year}-${month}-${day}`);
+          return {
+            barCode: linhaDigitavel,
+            amount,
+            dueDate,
+          };
+        }
+      }
+    }
+
     return {
       barCode: linhaDigitavel,
       amount,
-      dueDate,
     };
   }
 }
